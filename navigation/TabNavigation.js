9af26a88310f0fd875381e2eead99d92a586fb62
jodnddus <lockhost23@gmail.com>
2019-09-17 18:28:52 +0900

    stack navigator 생성 함수 리팩토링

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -6,23 +6,24 @@ import { createBottomTabNavigator } from "react-navigation-tabs";
 import { MoviesScreen, SearchScreen, TvScreen } from "../screens";
 import { TabBarIcon } from '../components/TabBarIcon';
 import { BG_COLOR } from "../constants/Colors";
+import { createStack } from './config';
 
 const TabNavigation = createBottomTabNavigator(
     {
     Movies: { 
-        screen: MoviesScreen,
+        screen: createStack(MoviesScreen, "Movie"),
         navigationOptions: {
             tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS == "ios" ? "ios-film" : "md-film"} />
         }
     },
     Search: {
-        screen: SearchScreen,
+        screen: createStack(SearchScreen, "Search"),
         navigationOptions: {
             tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS == "ios" ? "ios-tv" : "md-tv"}/>
         }
     },
     Tv: { 
-        screen: TvScreen, 
+        screen: createStack(TvScreen, "Tv"),
         navigationOptions: {
             tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name={Platform.OS == "ios" ? "ios-search" : "md-search"}/>
         }
