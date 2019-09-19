09faf1e0f152ea202e962af2b630ae9c1a73f7c2
jodnddus <lockhost23@gmail.com>
2019-09-19 09:52:34 +0900

    영화 화면 완성

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -6,6 +6,8 @@ import styled from "styled-components";
 import Loader from "../../components/Loader";
 import MovieSlider from "../../components/MovieSlider";
 import { BG_COLOR } from "../../constants/Colors";
+import Section from "../../components/Section";
+import MovieItem from "../../components/MovieItem";
 
 const Container = styled.ScrollView`
   background-color: ${BG_COLOR};
@@ -16,7 +18,39 @@ const MoviesPresenter = ({ loading, upcoming, popular, nowPlaying }) =>
     <Loader />
   ) : (
     <Container>
-      <MovieSlider movies={nowPlaying} />
+      {nowPlaying ? <MovieSlider movies={nowPlaying} /> : null}
+      {upcoming ? (
+        <Section movies={upcoming} title="Upcoming Movies">
+          {upcoming
+            .filter(movie => movie.poster_path !== null)
+            .map(movie => (
+              <MovieItem
+                key={movie.id}
+                id={movie.id}
+                posterPhoto={movie.poster_path}
+                title={movie.title}
+                voteAvg={movie.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+      {popular ? (
+        <Section horizontal={false} movies={upcoming} title="Popular Movies">
+          {popular
+            .filter(movie => movie.poster_path !== null)
+            .map(movie => (
+              <MovieItem
+                horizontal={true}
+                key={movie.id}
+                id={movie.id}
+                posterPhoto={movie.poster_path}
+                title={movie.title}
+                voteAvg={movie.vote_average}
+                overview={movie.overview}
+              />
+            ))}
+        </Section>
+      ) : null}
     </Container>
   );
 
