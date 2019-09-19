0d6a615521ecb7bc6d583d0a5234c04354843aa2
jodnddus <lockhost23@gmail.com>
2019-09-19 09:52:52 +0900

    tv화면 완성

--- screens/Tv/TvContainer.js
+++ screens/Tv/TvContainer.js
@@ -6,19 +6,19 @@ export default class extends React.Component {
   state = {
     loading: true,
     popular: null,
-    topRated: null,
+    airingThisWeek: null,
     airingToday: null
   };
 
   async componentDidMount() {
-    let popular, topRated, airingToday, error;
+    let popular, airingThisWeek, airingToday, error;
     try {
       ({
         data: { results: popular }
       } = await tvApi.popular());
       ({
-        data: { results: topRated }
-      } = await tvApi.topRated());
+        data: { results: airingThisWeek }
+      } = await tvApi.airingThisWeek());
       ({
         data: { results: airingToday }
       } = await tvApi.airingToday());
@@ -30,20 +30,20 @@ export default class extends React.Component {
         loading: false,
         error,
         popular,
-        topRated,
+        airingThisWeek,
         airingToday
       });
     }
   }
 
   render() {
-    const { loading, airingToday, topRated, popular } = this.state;
+    const { loading, airingToday, airingThisWeek, popular } = this.state;
     console.log(this.state);
     return (
       <TvPresenter
         loading={loading}
         airingToday={airingToday}
-        topRated={topRated}
+        airingThisWeek={airingThisWeek}
         popular={popular}
       />
     );
--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -1,11 +1,69 @@
 import React from "react";
 import { Text } from "react-native";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import Loader from "../../components/Loader";
+import Section from "../../components/Section";
+import { BG_COLOR } from "../../constants/Colors";
+import MovieItem from "../../components/MovieItem";
 
-const TvPresenter = ({ loading, popular, topRated, airingToday }) =>
-  loading ? <Loader /> : <Text>Tv</Text>;
+const Container = styled.ScrollView`
+  background-color: ${BG_COLOR};
+`;
+
+const TvPresenter = ({ loading, airingThisWeek, popular, airingToday }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {airingToday ? (
+        <Section title="Airing Today">
+          {airingToday
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+      {popular ? (
+        <Section title="Popular">
+          {popular
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+      {airingThisWeek ? (
+        <Section title="Airing This Week">
+          {airingThisWeek
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+    </Container>
+  );
 
 TvPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
