9525dbde9d94eebe0704d324a5a60881f084131a
jodnddus <lockhost23@gmail.com>
2019-09-19 16:03:56 +0900

    lint-staged 작동확인 #5

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -5,7 +5,7 @@ import TabNavigation from "./TabNavigation";
 import { DetailScreen } from "../screens";
 import { headerStyles } from "./config";
 
-const MainNavigation = createStackNavigator(
+const Mainnavigation = createStackNavigator(
   {
     Tabs: { screen: TabNavigation, navigationOptions: { header: null } },
     Detail: {
@@ -21,4 +21,4 @@ const MainNavigation = createStackNavigator(
   }
 );
 
-export default createAppContainer(MainNavigation);
+export default createAppContainer(Mainnavigation);
